777cb3bbd058f62795e1866498a06705b51b090f
campuslogic <alicia.davis@campuslogic.awesome>
2016-03-08 12:55:27 -0700

    CL Connect Initial Push
    Pushing CL connect changes to github, contains automated process for
    ISIR Corrections, Upload Services, Event Notifications, and email
    alerts.

---
+++ WebAPI/NotificationEventController.cs
@@ -0,0 +1,45 @@
+﻿using CampusLogicEvents.Web.Filters;
+using CampusLogicEvents.Web.Models;
+using log4net;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CampusLogicEvents.Web.WebAPI
+{
+    [IdentityBasicAuthentication] // Enable Basic authentication for this controller.
+    [Authorize] // Require authenticated requests.
+    public class NotificationEventController : ApiController
+    {
+        private static readonly ILog logger = LogManager.GetLogger("AdoNetAppender");
+
+        // POST api/NotificationEvent
+        public HttpResponseMessage Post([FromBody] IEnumerable<JObject> postData)
+        {
+            try
+            {
+                //Header verification "User-Agent: StudentVerification Event API"
+                if (Request.Headers.UserAgent.ToString() != "StudentVerification")
+                {
+                    Request.CreateResponse(HttpStatusCode.ExpectationFailed); // Anything but 200 is considered an error and StudentVerification will retry to send it.
+                }
+
+                if (postData != null)
+                {
+                    //Store the received event
+                    DataService.ReceivePostedEvents(postData);
+                }
+                
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorFormat("NotificationEvent Post Error: {0}", ex);
+                return Request.CreateResponse(HttpStatusCode.ExpectationFailed);
+            }
+        }
+    }
+}
