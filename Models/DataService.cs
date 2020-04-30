16dfa42cfde45a9cba94224663598c9aa7a055e3
campuslogic <github@campuslogic.com>
2020-04-30 17:24:47 +0000

    CLConnect 2.12.5.29259

--- Models/DataService.cs
+++ Models/DataService.cs
@@ -379,6 +379,9 @@ namespace CampusLogicEvents.Web.Models
                 // Parse parameters based on config.
                 List<OdbcParameter> parameters =
                     storedProcedureSettings.GetParameters().Select(p => ParseParameter(p, eventData)).ToList();
+                
+                //Adding logging in case client experiences weird argument error, we can better determine what we are trying to pass
+                logger.Info($"Parameters to be pass into database: { String.Join(", ", parameters.Select(x => x.ParameterName + ": " + x.Value.ToString() + " - DataType: " + Enum.GetName(typeof(OdbcType), x.OdbcType)))}");
 
                 // For each parameter, need to add a placeholder "?" in the sql command.  
                 // This is just part of the ODBC syntax.
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CampusLogicEvents.Web")]
-[assembly: AssemblyDescription("Build: 2.12.4.24079 - Commit: 20465")]
+[assembly: AssemblyDescription("Build: 2.12.5.29259 - Commit: 20661")]
 [assembly: AssemblyConfiguration("release")]
 [assembly: AssemblyCompany("CampusLogic")]
 [assembly: AssemblyProduct("CL Connect")]
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.12.4")]
-[assembly: AssemblyFileVersion("2.12.4.0")]
+[assembly: AssemblyVersion("2.12.5")]
+[assembly: AssemblyFileVersion("2.12.5.0")]
 [assembly: log4net.Config.XmlConfigurator]
-[assembly: AssemblyInformationalVersion("2.12.4.0")]
+[assembly: AssemblyInformationalVersion("2.12.5.0")]
